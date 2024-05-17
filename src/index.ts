11a5ffffdc34bb1e4b21b62ec2b6ef095138959d
pozy <masikapolycarp@gmail.com>
2024-05-17 22:04:41 +0300

    bun build

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ app.get("/", (c) => {
   return c.html(html`<h1>Hello, from Hono!</h1>`);
 });
 
+console.log("Server is running on port 3000");
+
 export default {
   port: process.env.PORT || 3000,
   fetch: app.fetch,
